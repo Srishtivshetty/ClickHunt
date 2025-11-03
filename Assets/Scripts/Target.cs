66c5b78d12ec47257504e63a5c8253def887d666
Srishtivshetty <srishti.shetty@indivillage.co>
2025-11-03 15:23:38 +0530

    Adding Particle explosion

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -11,6 +11,7 @@ public class Target : MonoBehaviour
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -6;
+    public ParticleSystem explosionParticle;
     public int pointValue;
     // Start is called before the first frame update
     void Start()
@@ -32,6 +33,7 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
         gameManager.UpdateScore(pointValue);
     }
 
