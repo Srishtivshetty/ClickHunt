07cee262a4e661d39aa507b052c7bc43558ee47a
Srishtivshetty <srishti.shetty@indivillage.co>
2025-11-04 09:40:31 +0530

    Adding dificulty  button script

--- Assets/Scripts/DifficultyButton.cs
+++ Assets/Scripts/DifficultyButton.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour
+public class DifficultyButton : MonoBehaviour
 {
+    private Button button;
     // Start is called before the first frame update
     void Start()
     {
-        
+        button = GetComponent<Button>();
     }
 
     // Update is called once per frame
