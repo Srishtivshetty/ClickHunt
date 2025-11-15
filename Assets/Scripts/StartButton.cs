20c9cc47426677452beef327065c44392d5059ea
Srishtivshetty <srishti.shetty@indivillage.co>
2025-11-15 11:09:56 +0530

    Adding comment to StartButton

--- Assets/Scripts/StartButton.cs
+++ Assets/Scripts/StartButton.cs
@@ -3,12 +3,15 @@ using UnityEngine.UI;
 
 public class StartButtonScript : MonoBehaviour
 {
-    private Button button;
-    public LobbyEconomy lobbyEconomy; // Correct reference
+    private Button button;            // Reference to the UI Button component
+    public LobbyEconomy lobbyEconomy; // Reference to the LobbyEconomy script (assigned in Inspector)
 
     void Start()
     {
+        // Get the Button component on this GameObject
         button = GetComponent<Button>();
+
+        // Register click event -> runs OnPlayButtonClicked() when pressed
         button.onClick.AddListener(OnPlayButtonClicked);
     }
 
